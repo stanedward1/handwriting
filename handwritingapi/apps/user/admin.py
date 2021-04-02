f879fe6edf0fb0b5e4d6b5ed3c1144d3cea5f31e
stanedward1 <1679648995@qq.com>
2021-04-03 00:38:26 +0800

    修改后台管理网页title&站点header

--- handwritingapi/apps/user/admin.py
+++ handwritingapi/apps/user/admin.py
@@ -7,3 +7,8 @@ admin.site.register(models.User)
 class Meta:
     verbose_name = "用户"
     verbose_name_plural = "用户"
+
+
+# 修改网页title和站点header。
+admin.site.site_title = "间架笔画后台管理平台"
+admin.site.site_header = "间架笔画后台管理平台"
